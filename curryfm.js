aac7d4100449a84b9034b79b3ebfd95c75c9a155
Sid <sid26@ucla.edu>
2016-04-30 18:23:14 -0700

    remove name param

--- curryfm.js
+++ curryfm.js
@@ -1,11 +1,11 @@
 import R from 'ramda';
 import http from 'http';
 
-const lastfm = (api_key, resource, name, method, params, callback) => {
+const lastfm = (api_key, resource, method, params, callback) => {
 	const prependAndKeyEquals = R.mapObjIndexed((value, key, object) => `&${key}=${value}`);
 	const add = (a, b) => a + b;
 	const paramStr = R.compose(R.reduce(add, ''), R.values, prependAndKeyEquals);
-	const queryStr = `http://ws.audioscrobbler.com/2.0/?api_key=${api_key}&method=${resource}.${method}&${resource}=${name}${paramStr(params)}`;
+	const queryStr = `http://ws.audioscrobbler.com/2.0/?api_key=${api_key}&method=${resource}.${method}${paramStr(params)}`;
 	http.get(queryStr, (res) => {
 			let body = '';
 			res.on('data', (chunk) => {
