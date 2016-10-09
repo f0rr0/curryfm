64e1994b341b5066ceee49526f14f7a2c038d660
Sid <sid26@ucla.edu>
2016-10-10 04:12:36 +0530

    Update build

--- build.js
+++ build.js
@@ -14,7 +14,7 @@ var _http2 = _interopRequireDefault(_http);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var lastfm = function lastfm(api_key, resource, method, params, callback) {
+function lastfm(api_key, resource, method, params) {
 	var prependAndKeyEquals = _ramda2.default.mapObjIndexed(function (value, key, object) {
 		return '&' + key + '=' + value;
 	});
@@ -23,18 +23,19 @@ var lastfm = function lastfm(api_key, resource, method, params, callback) {
 	};
 	var paramStr = _ramda2.default.compose(_ramda2.default.reduce(add, ''), _ramda2.default.values, prependAndKeyEquals);
 	var queryStr = 'http://ws.audioscrobbler.com/2.0/?api_key=' + api_key + '&method=' + resource + '.' + method + paramStr(params);
-	_http2.default.get(queryStr, function (res) {
-		var body = '';
-		res.on('data', function (chunk) {
-			body += chunk;
+	return new Promise(function (resolve, reject) {
+		_http2.default.get(queryStr, function (res) {
+			var body = '';
+			res.on('data', function (chunk) {
+				body += chunk;
+			});
+			res.on('end', function () {
+				resolve(JSON.parse(body));
+			});
+		}).on('error', function (err) {
+			reject(err);
 		});
-		res.on('end', function () {
-			callback(JSON.parse(body), null);
-		});
-	}).on('error', function (err) {
-		callback(null, err);
 	});
 };
 
-var curryfm = _ramda2.default.curry(lastfm);
-exports.default = curryfm;
+exports.default = _ramda2.default.curry(lastfm);
